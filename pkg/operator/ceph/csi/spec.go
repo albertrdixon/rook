c01d80581db6c25d3695ff206fe65fe0c0910d20
Rakshith R <rar@redhat.com>
2021-10-13 14:14:40 +0530

    ceph: apply csi provisioner node-affinity to csi version check job
    This commit makes sure csi provisioner node-affinity is applied
    to the csi version check job as well, similar to the how
    the existing csi provisioner toleration is applied to the job.
    
    Fixes: #8323
    
    Signed-off-by: Rakshith R <rar@redhat.com>
    (cherry picked from commit c1ef189b9199d57649cf47bcb5fc7d747ef13f9e)
    
    # Conflicts:
    #	pkg/operator/ceph/csi/spec.go

--- pkg/operator/ceph/csi/spec.go
+++ pkg/operator/ceph/csi/spec.go
@@ -726,8 +726,16 @@ func validateCSIVersion(clientset kubernetes.Interface, namespace, rookImage, se
 	job := versionReporter.Job()
 	job.Spec.Template.Spec.ServiceAccountName = serviceAccountName
 
+<<<<<<< HEAD
 	// Apply csi provisioner toleration for csi version check job
 	job.Spec.Template.Spec.Tolerations = getToleration(clientset, provisionerTolerationsEnv, []corev1.Toleration{})
+=======
+	// Apply csi provisioner toleration and affinity for csi version check job
+	job.Spec.Template.Spec.Tolerations = getToleration(r.opConfig.Parameters, provisionerTolerationsEnv, []corev1.Toleration{})
+	job.Spec.Template.Spec.Affinity = &corev1.Affinity{
+		NodeAffinity: getNodeAffinity(r.opConfig.Parameters, provisionerNodeAffinityEnv, &corev1.NodeAffinity{}),
+	}
+>>>>>>> c1ef189b9 (ceph: apply csi provisioner node-affinity to csi version check job)
 	stdout, _, retcode, err := versionReporter.Run(timeout)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to complete ceph CSI version job")
